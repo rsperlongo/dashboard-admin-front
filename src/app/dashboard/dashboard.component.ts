e4fbf02442867186bfd1afb80458d0079cfb178b
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2023-07-25 16:15:34 -0300

    update dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../models';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  private userSubject: BehaviorSubject<User | null>;
+  public user: Observable<User | null>;
+
+  constructor(private router: Router) {
+    this.userSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('user')!));
+    this.user = this.userSubject.asObservable();
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.userSubject.next(null);
+    this.router.navigate(['login']);
+  }
 
 }
