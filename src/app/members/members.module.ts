e0e323f1a15207487db045d5bd72744ecfd7550b
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2023-09-24 16:00:59 -0300

    create members components

---
+++ src/app/members/members.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MembersComponent } from './members.component';
+
+
+
+@NgModule({
+  declarations: [
+    MembersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MembersModule { }
